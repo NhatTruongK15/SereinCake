13e895a3937dd79b0bd9e1c2b50d29278d3d669c
NhatTruongK15 <20522086@gm.uit.edu.vn>
2022-11-15 00:26:53 +0700

    Home Page

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+let search = document.querySelector('.search');
+document.querySelector('#search').onclick=()=> { 
+    search.classList.toggle('active');
+}
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu').onclick=() =>{
+    navbar.classList.toggle('active');
+}
